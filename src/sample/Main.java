9263cb40c1b4be14966d43a93a1195a2db4eabf2
User <User@WIN-T9277J17Q45>
2018-06-08 23:28:15 +0400

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+public class Main extends Application {
+
+    //public static Socket socket;
+
+    //public TextField textField;
+    //public Button sendMessage;
+
+    @Override
+
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
+    }
+}
